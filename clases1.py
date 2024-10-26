138385d741522839fbd302e86430e227af14b8ed
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-25 20:30:10 -0600

    Add files via upload

---
+++ clases1.py
@@ -0,0 +1,7 @@
+class alumno:
+    def __init__(self, num, apellido, nombre, materia, califa ):
+        self.num = num 
+        self.apellido = apellido 
+        self.nombre = nombre 
+        self.materia = materia 
+        self.califa = califa 
---
+++ onjeto.py
@@ -0,0 +1,7 @@
+from clases1 import alumno
+obj = alumno(12,"ramirez", "manzo","PM3",100)
+print(obj.num)
+print(obj.apellido)
+print(obj.nombre)
+print(obj.materia)
+print(obj.califa)
